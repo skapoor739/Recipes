94aed22daa43d8346cf8395fd535d24302eb337e
Shivam Kapur <Shivam@shivams-macbook-pro.local>
2015-11-28 14:39:50 +0530

    Initial Commit

---
+++ Recipies-CoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recipies-CoreData
+//
+//  Created by Shivam Kapur on 28/11/15.
+//  Copyright © 2015 Shivam Kapur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
